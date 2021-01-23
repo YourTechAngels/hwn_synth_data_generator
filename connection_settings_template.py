6739d5288670924e943ef84815f91fa777804c47
Nathalie Rud <nathalier@gmail.com>
2021-01-23 23:46:05 +0000

    implement populating aws mysql db

---
+++ connection_settings_template.py
@@ -0,0 +1,8 @@
+db_params_aws_mysql = {
+ 'dialect': 'mysql+pymysql',
+ 'host': 'string',
+ 'port': 3306,
+ 'database': 'string',
+ 'user': 'string',
+ 'password': 'string'
+}
--- populate_db.py
+++ populate_db.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 import pandas as pd
+from connection_settings import *
 
 
 def clear_tables(engine):
@@ -36,11 +37,6 @@ def populate(engine):
     df_task_types.to_sql(name='tasks_tasktype', con=engine, index=False,  if_exists='append', method='multi')
     print(f'{len(df_task_types)} task_type entries have been inserted')
 
-    df_tasks = pd.read_csv('generated/tasks.csv')
-    df_tasks.index += 1
-    df_tasks.to_sql(name='tasks_task', con=engine, index=True, index_label="id",  if_exists='append', method='multi')
-    print(f'{len(df_tasks)} task entries have been inserted')
-
     df_users = pd.read_csv('generated/users.csv')
     not_null_char_cols = "password,first_name,last_name,username,email,phone_number,post_code,address_line_1,address_line_2,city,county".split(
         ",")
@@ -58,21 +54,35 @@ def populate(engine):
     except FileNotFoundError:
         print('No saved users to add.')
 
+    df_tasks = pd.read_csv('generated/tasks.csv')
+    df_tasks.index += 1
+    df_tasks.to_sql(name='tasks_task', con=engine, index=True, index_label="id",  if_exists='append', method='multi')
+    print(f'{len(df_tasks)} task entries have been inserted')
+
 
 if __name__ == "__main__":
-    to_clear_db = False
-    db_path = 'db.sqlite3'
+    to_clear_db = True
+    eng_conf, db_path = '', ''
 
     # parse arguments
     if len(sys.argv) > 1:
         args_to_parse = sys.argv[1:]
+        # -d option specify that db is not empty and thus all data should be
+        # deleted before inserting new data
         if "-d" in args_to_parse:
             args_to_parse.remove("-d")
             to_clear_db = True
-        if args_to_parse:
+        # instruction to populate remote db instance
+        if '--aws' in args_to_parse:
+            db_params = db_params_aws_mysql
+            eng_conf = f"{db_params['dialect']}://{db_params['user']}:"\
+                       f"{db_params['password']}@{db_params['host']}/{db_params['database']}"
+        # path to local sqlite db file
+        elif args_to_parse:
             db_path = args_to_parse[0]
 
-    engine = create_engine(r'sqlite:///' + db_path)
+    eng_conf = eng_conf or (r'sqlite:///' + (db_path or 'db.sqlite3'))
+    engine = create_engine(eng_conf)
     if to_clear_db:
         clear_tables(engine)
     sys.exit(populate(engine))
